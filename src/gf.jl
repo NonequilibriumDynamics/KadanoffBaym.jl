77cb766ee45025fb472396ccf24118921940a13f
Francisco Meirinhos <fcmeirinhos@gmail.com>
2019-09-27 10:02:29 +0200

    Green functions now support multidimensional indexing

--- src/gf.jl
+++ src/gf.jl
@@ -6,62 +6,83 @@ abstract type GreenFunction end
 # Defined as
 #     G^<(t,t') = -i < a^{\dagger}(t) a(t') > for t > t'
 # """
-struct LesserGF{T,S<:AbstractMatrix{<:T}} <: GreenFunction
+struct LesserGF{T,S<:AbstractArray{<:T}} <: GreenFunction
     data::S
 
-    function LesserGF{T,S}(data) where {T, S<:AbstractMatrix{<:T}}
+    function LesserGF{T,S}(data) where {T, S<:AbstractArray{<:T}}
+        # Base.require_one_based_indexing(data)
         new{T,S}(copy(data))
     end
 end
 
-function LesserGF(A::AbstractMatrix)
+function LesserGF(A::AbstractArray)
     LinearAlgebra.checksquare(A)
     return lesserGF_type(typeof(A))(A)
 end
 
-function lesserGF_type(::Type{T}) where {S<:Number, T<:AbstractMatrix{S}}
+function LesserGF(L::Integer)
+    return LesserGF(zeros(Complex{Float64}, L, L))
+end
+
+function lesserGF_type(::Type{T}) where {S<:Number, T<:AbstractArray{S}}
     return LesserGF{S, T}
 end
-function lesserGF_type(::Type{T}) where {S<:AbstractMatrix, T<:AbstractMatrix{S}}
-    return LesserGF{AbstractMatrix, T}
+function lesserGF_type(::Type{T}) where {S<:AbstractArray, T<:AbstractArray{S}}
+    return LesserGF{AbstractArray, T}
 end
 
 # """
 # Defined as
 #     G^>(t,t') = +i < a^{\dagger}(t') a(t) > for t <= t'
 # """
-struct GreaterGF{T,S<:AbstractMatrix{<:T}} <: GreenFunction
+struct GreaterGF{T,S<:AbstractArray{<:T}} <: GreenFunction
     data::S
 
-    function GreaterGF{T,S}(data) where {T, S<:AbstractMatrix{<:T}}
+    function GreaterGF{T,S}(data) where {T, S<:AbstractArray{<:T}}
+        # Base.require_one_based_indexing(data)
         new{T,S}(copy(data))
     end
 end
 
-function GreaterGF(A::AbstractMatrix)
+function GreaterGF(A::AbstractArray)
     LinearAlgebra.checksquare(A)
     return greaterGF_type(typeof(A))(A)
 end
 
-function greaterGF_type(::Type{T}) where {S<:Number, T<:AbstractMatrix{S}}
+function GreaterGF(::T, L::Integer) where T
+    return GreaterGF(zeros(T, L, L))
+end
+
+function greaterGF_type(::Type{T}) where {S<:Number, T<:AbstractArray{S}}
     return GreaterGF{S, T}
 end
-function greaterGF_type(::Type{T}) where {S<:AbstractMatrix, T<:AbstractMatrix{S}}
-    return GreaterGF{AbstractMatrix, T}
+function greaterGF_type(::Type{T}) where {S<:AbstractArray, T<:AbstractArray{S}}
+    return GreaterGF{AbstractArray, T}
 end
 
 const LesserOrGreater{T,S} = Union{LesserGF{T,S}, GreaterGF{T,S}}
 
 size(G::LesserOrGreater) = size(G.data)
 
-@inline function Base.getindex(G::LesserOrGreater, i::Integer, j::Integer)
+@inline function Base.getindex(G::LesserOrGreater, i::Integer, j::Integer) where {N}
     # @boundscheck Base.checkbounds(G.data, i, j)
     @inbounds if i == j
-        return G.data[i, j]
+        return getindex(G.data, i, j)
     elseif (G isa LesserGF) == (i > j)
-        return G.data[i, j]
+        return getindex(G.data, i, j)
     else
-        return -adjoint(G.data[j, i])
+        return -adjoint(getindex(G.data, j, i))
+    end
+end
+
+@inline function Base.getindex(G::LesserOrGreater, i::Integer, j::Integer, I...)
+    # @boundscheck Base.checkbounds(G.data, i, j)
+    @inbounds if i == j
+        return getindex(G.data, i, j, I...)
+    elseif (G isa LesserGF) == (i > j)
+        return getindex(G.data, i, j, I...)
+    else
+        return -adjoint(getindex(G.data, j, i, I...))
     end
 end
 
@@ -75,6 +96,19 @@ function Base.setindex!(G::LesserOrGreater, v, i::Integer, j::Integer)
     # else 
     #     setindex!(G.data, v, i, j)
     #     setindex!(G.data, -adjoint(v), j, i)
+    end
+end
+
+function Base.setindex!(G::LesserOrGreater, v, i::Integer, j::Integer, I...)
+    @inbounds if i == j
+        setindex!(G.data, v, i, j, I...)
+    elseif (G isa LesserGF) == (i > j)
+        setindex!(G.data, v, i, j, I...)
+    else
+        setindex!(G.data, -adjoint(v), j, i, I...)
+    # else 
+    #     setindex!(G.data, v, i, j, I...)
+    #     setindex!(G.data, -adjoint(v), j, i, I...)
     end
 end
 
--- test/gf_test.jl
+++ test/gf_test.jl
@@ -11,8 +11,8 @@ lgf = LesserGF(data)
 ggf = GreaterGF(data)
 
 v = 30 + 30im
-lgf[2, N] = 30 + 30im
-ggf[2, N] = 30 + 30im
+lgf[2, N] = v
+ggf[2, N] = v
 
 @test lgf[N, 2] == -adjoint(v)
 @test ggf[N, 2] == -adjoint(v)
@@ -24,3 +24,23 @@ ggf[2, N] = 30 + 30im
 @test lgf.data[2, N] != lgf.data[N, 2]
 @test ggf.data[2, N] != ggf.data[N, 2]
 
+
+data = rand(Complex{Float64}, N, N, N, N)
+data -= permutedims(data, [1,2,4,3])
+
+lgf = LesserGF(data)
+ggf = GreaterGF(data)
+
+v = rand(Complex{Float64}, N, N)
+lgf[2, N, :, :] = v
+ggf[2, N, :, :] = v
+
+@test lgf[N, 2, :, :] == -adjoint(v)
+@test ggf[N, 2, :, :] == -adjoint(v)
+
+@test lgf[2, N, :, :] == -adjoint(lgf[N, 2, :, :])
+@test ggf[2, N, :, :] == -adjoint(ggf[N, 2, :, :])
+
+# For special index set
+@test lgf.data[2, N, :, :] != lgf.data[N, 2, :, :]
+@test ggf.data[2, N, :, :] != ggf.data[N, 2, :, :]
