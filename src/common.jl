e4236e037b37bddd0c2732b6cb828602a7e92bbe
Francisco Meirinhos <fcmeirinhos@gmail.com>
2019-10-01 17:16:05 +0200

    Changes to DiffEqBase.__init of a KadanoffBaym problem

--- src/common.jl
+++ src/common.jl
@@ -4,8 +4,6 @@ export KB
 OrdinaryDiffEq.alg_order(::KB{algType}) where {algType} = OrdinaryDiffEq.alg_order(algType())
 OrdinaryDiffEq.isfsal(::KB) = false
 
-cachify(::KB{algType}) where algType = eval(Symbol(string(algType) * "ConstantCache"))
-
 # OrdinaryDiffEq.@cache
 mutable struct KBCache{algCacheType} <: OrdinaryDiffEq.OrdinaryDiffEqConstantCache
   caches::Array{algCacheType, 1}
@@ -13,8 +11,10 @@ end
 
 function OrdinaryDiffEq.alg_cache(::KB{algType},u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol_internal,p,calck,::Type{Val{false}}) where algType
   cache = OrdinaryDiffEq.alg_cache(algType(),u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol_internal,p,calck,Val{false})
-  return KBCacheCache{cachify(algType)}()
+  return KBCache{typeof(cache)}([cache])
 end
+
+mutable struct KBIntegrator end
 
 # Code much like OrdinaryDiffEq.jl, which is licensed under the MIT "Expat" License
 function DiffEqBase.__init(prob::ODEProblem, alg::KB, dt::Float64,
@@ -45,31 +45,31 @@ function DiffEqBase.__init(prob::ODEProblem, alg::KB, dt::Float64,
   tTypeNoUnits   = typeof(one(tType))
   
   if abstol === nothing
-  if uBottomEltypeNoUnits == uBottomEltype
-    abstol_internal = real(convert(uBottomEltype,oneunit(uBottomEltype)*1//10^6))
+    if uBottomEltypeNoUnits == uBottomEltype
+      abstol_internal = real(convert(uBottomEltype,oneunit(uBottomEltype)*1//10^6))
+    else
+      abstol_internal = real.(oneunit.(u).*1//10^6)
+    end
   else
-    abstol_internal = real.(oneunit.(u).*1//10^6)
-  end
-  else
-  abstol_internal = real.(abstol)
+    abstol_internal = real.(abstol)
   end
   
   if reltol === nothing
-  if uBottomEltypeNoUnits == uBottomEltype
-    reltol_internal = real(convert(uBottomEltype,oneunit(uBottomEltype)*1//10^3))
+    if uBottomEltypeNoUnits == uBottomEltype
+      reltol_internal = real(convert(uBottomEltype,oneunit(uBottomEltype)*1//10^3))
+    else
+      reltol_internal = real.(oneunit.(u).*1//10^3)
+    end
   else
-    reltol_internal = real.(oneunit.(u).*1//10^3)
-  end
-  else
-  reltol_internal = real.(reltol)
+    reltol_internal = real.(reltol)
   end
   
   dtmax > zero(dtmax) && tdir < 0 && (dtmax *= tdir) # Allow positive dtmax, but auto-convert
   
   if uBottomEltypeNoUnits == uBottomEltype
-  rate_prototype = u
+    rate_prototype = u
   else # has units!
-  rate_prototype = u/oneunit(tType)
+    rate_prototype = u/oneunit(tType)
   end
   
   rateType = typeof(rate_prototype)
@@ -89,4 +89,62 @@ function DiffEqBase.__init(prob::ODEProblem, alg::KB, dt::Float64,
   else
     callback_cache = nothing
   end
+  
+  ksEltype = Vector{rateType}
+  timeseries_init = typeof(prob.u0)[]
+  timeseries = convert(Vector{uType},timeseries_init)
+
+  ts_init = eltype(prob.tspan)[]
+  ts = convert(Vector{tType},ts_init)
+  
+  ks_init = []
+  ks = convert(Vector{ksEltype},ks_init)
+  alg_choice = Int[]
+    
+  adptive = OrdinaryDiffEq.isadaptive(alg)
+  if !adptive
+    steps = ceil(Int,(tspan[2]-tspan[1])/dt)
+    sizehint!(timeseries, steps+1)
+    sizehint!(ts,steps+1)
+    sizehint!(ks,steps+1)
+  else
+    @assert false
+  end
+  
+  copyat_or_push!(ts,1,t)
+  copyat_or_push!(timeseries,1,u)
+  copyat_or_push!(ks,1,[rate_prototype])
+    
+  qmin = OrdinaryDiffEq.qmin_default(alg)
+  QT = tTypeNoUnits <: Integer ? typeof(qmin) : tTypeNoUnits
+    
+  k = rateType[]
+    
+  calck = (callback !== nothing && callback != CallbackSet()) || # Empty callback
+                                   (prob.callback !== nothing && prob.callback != CallbackSet()) || # Empty prob.callback
+                                   (!isempty(setdiff(saveat,tstops)) || true), # and no dense output
+
+  if OrdinaryDiffEq.uses_uprev(alg, adptive) || calck
+    uprev = recursivecopy(u)
+  else
+    uprev = u
+  end
+
+  if !OrdinaryDiffEq.alg_extrapolates(alg)
+    uprev2 = uprev
+  end
+    
+  cache = OrdinaryDiffEq.alg_cache(alg,u,rate_prototype,uEltypeNoUnits,uBottomEltypeNoUnits,tTypeNoUnits,uprev,uprev2,f,t,dt,reltol_internal,p,calck,Val{isinplace(prob)})
+
+  id = OrdinaryDiffEq.InterpolationData(f,timeseries,ts,ks,true,cache)
+  beta2 = OrdinaryDiffEq.beta2_default(alg)
+  beta1 = OrdinaryDiffEq.beta1_default(alg,beta2)
+end
+
+function OrdinaryDiffEq.initialize!(integrator,cache::KBCache)
+  assert false
+end
+
+function OrdinaryDiffEq.perform_step!(integrator,cache::KBCache,repeat_step=false)
+  @unpack t, dt = integrator
 end
