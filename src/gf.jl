572e1f5aa80562f37361ae0b8335e6cf63ab13c7
Francisco Meirinhos <fcmeirinhos@gmail.com>
2019-10-21 15:11:55 +0200

    Changed formatting in gf.jl

--- src/gf.jl
+++ src/gf.jl
@@ -5,24 +5,24 @@ abstract type GreenFunction end
 #     G^<(t,t') = -i < a^{\dagger}(t) a(t') > for t > t'
 # """
 struct LesserGF{T,S<:AbstractArray{<:T}} <: GreenFunction
-    data::S
+  data::S
 
-    function LesserGF{T,S}(data) where {T, S<:AbstractArray{<:T}}
-        # Base.require_one_based_indexing(data)
-        new{T,S}(copy(data))
-    end
+  function LesserGF{T,S}(data) where {T, S<:AbstractArray{<:T}}
+    # Base.require_one_based_indexing(data)
+    new{T,S}(copy(data))
+  end
 end
 
 function LesserGF(A::AbstractArray)
-    LinearAlgebra.checksquare(A)
-    return lesserGF_type(typeof(A))(A)
+  LinearAlgebra.checksquare(A)
+  return lesserGF_type(typeof(A))(A)
 end
 
 function lesserGF_type(::Type{T}) where {S<:Number, T<:AbstractArray{S}}
-    return LesserGF{S, T}
+  return LesserGF{S, T}
 end
 function lesserGF_type(::Type{T}) where {S<:AbstractArray, T<:AbstractArray{S}}
-    return LesserGF{AbstractArray, T}
+  return LesserGF{AbstractArray, T}
 end
 
 # """
@@ -30,24 +30,24 @@ end
 #     G^>(t,t') = +i < a^{\dagger}(t') a(t) > for t <= t'
 # """
 struct GreaterGF{T,S<:AbstractArray{<:T}} <: GreenFunction
-    data::S
+  data::S
 
-    function GreaterGF{T,S}(data) where {T, S<:AbstractArray{<:T}}
-        # Base.require_one_based_indexing(data)
-        new{T,S}(copy(data))
-    end
+  function GreaterGF{T,S}(data) where {T, S<:AbstractArray{<:T}}
+    # Base.require_one_based_indexing(data)
+    new{T,S}(copy(data))
+  end
 end
 
 function GreaterGF(A::AbstractArray)
-    LinearAlgebra.checksquare(A)
-    return greaterGF_type(typeof(A))(A)
+  LinearAlgebra.checksquare(A)
+  return greaterGF_type(typeof(A))(A)
 end
 
 function greaterGF_type(::Type{T}) where {S<:Number, T<:AbstractArray{S}}
-    return GreaterGF{S, T}
+  return GreaterGF{S, T}
 end
 function greaterGF_type(::Type{T}) where {S<:AbstractArray, T<:AbstractArray{S}}
-    return GreaterGF{AbstractArray, T}
+  return GreaterGF{AbstractArray, T}
 end
 
 const LesserOrGreater{T,S} = Union{LesserGF{T,S}, GreaterGF{T,S}}
@@ -58,51 +58,53 @@ Base.eltype(::Type{<:LesserOrGreater{T,S}}) where {T,S} = eltype(S)
 Base.convert(T::Type{<:GreenFunction}, m::AbstractArray) = T(m)
 
 @inline function Base.getindex(G::LesserOrGreater, i::Integer, j::Integer) where {N}
-    # @boundscheck Base.checkbounds(G.data, i, j)
-    @inbounds if i == j
-        return getindex(G.data, i, j)
-    elseif (G isa LesserGF) == (i > j)
-        return getindex(G.data, i, j)
-    else
-        return -adjoint(getindex(G.data, j, i))
-    end
+  # @boundscheck Base.checkbounds(G.data, i, j)
+  # @inbounds if i == j
+  #     return getindex(G.data, i, j)
+  # elseif (G isa LesserGF) == (i > j)
+  #     return getindex(G.data, i, j)
+  # else
+  #     return -adjoint(getindex(G.data, j, i))
+  # end
+  getindex(G.data, i, j)
 end
 
 @inline function Base.getindex(G::LesserOrGreater, i::Integer, j::Integer, I...)
-    # @boundscheck Base.checkbounds(G.data, i, j)
-    @inbounds if i == j
-        return getindex(G.data, i, j, I...)
-    elseif (G isa LesserGF) == (i > j)
-        return getindex(G.data, i, j, I...)
-    else
-        return -adjoint(getindex(G.data, j, i, I...))
-    end
+  # @boundscheck Base.checkbounds(G.data, i, j)
+  # @inbounds if i == j
+  #     return getindex(G.data, i, j, I...)
+  # elseif (G isa LesserGF) == (i > j)    
+  #     return getindex(G.data, i, j, I...)
+  # else
+  #     return -adjoint(getindex(G.data, j, i, I...))
+  # end
+  getindex(G.data, i, j, I...)
 end
 
 function Base.setindex!(G::LesserOrGreater, v, i::Integer, j::Integer)
-    @inbounds if i == j
-        setindex!(G.data, v, i, j)
-    elseif (G isa LesserGF) == (i > j)
-        setindex!(G.data, v, i, j)
-    else
-        setindex!(G.data, -adjoint(v), j, i)
-    # else 
-    #     setindex!(G.data, v, i, j)
-    #     setindex!(G.data, -adjoint(v), j, i)
-    end
+  @inbounds if i == j
+    setindex!(G.data, v, i, j)
+  # elseif (G isa LesserGF) == (i > j)
+  #     setindex!(G.data, v, i, j)
+  # else
+  #     setindex!(G.data, -adjoint(v), j, i)
+  else 
+    setindex!(G.data, v, i, j)
+    setindex!(G.data, -adjoint(v), j, i)
+  end
 end
 
 function Base.setindex!(G::LesserOrGreater, v, i::Integer, j::Integer, I...)
-    @inbounds if i == j
-        setindex!(G.data, v, i, j, I...)
-    elseif (G isa LesserGF) == (i > j)
-        setindex!(G.data, v, i, j, I...)
-    else
-        setindex!(G.data, -adjoint(v), j, i, I...)
-    # else 
-    #     setindex!(G.data, v, i, j, I...)
-    #     setindex!(G.data, -adjoint(v), j, i, I...)
-    end
+  @inbounds if i == j
+    setindex!(G.data, v, i, j, I...)
+  # elseif (G isa LesserGF) == (i > j)
+  #     setindex!(G.data, v, i, j, I...)
+  # else
+  #     setindex!(G.data, -adjoint(v), j, i, I...)
+  else 
+    setindex!(G.data, v, i, j, I...)
+    setindex!(G.data, -adjoint(v), j, i, I...)
+  end
 end
 
 # struct RetardedGF <: GreenFunction end
