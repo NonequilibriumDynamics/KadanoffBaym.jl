85dd4da0c346e16683dcd61bb52a219c9220b061
Francisco Meirinhos <fcmeirinhos@gmail.com>
2020-04-03 15:21:55 +0200

    Added trapz to utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -1,16 +1,45 @@
+# Trapezoidal integration rule
+function trapz(x::Vector, y::Vector)
+  n = length(x)
+  @assert n == length(y) 
+
+  r = zero(promote_type(eltype(y), eltype(x)))
+  
+  if n == 1; return r; end
+  
+  for i in 2:n
+    r += (x[i] - x[i-1]) * (y[i] + y[i-1])
+  end
+  
+  return r/2
+end
+
+# Trapezoidal integration rule
+function trapz(x::Vector, y::Vector, i::Int, j::Int)
+  r = zero(promote_type(eltype(y), eltype(x)))
+  
+  if i == j; return r; end
+  
+  for k in 1+min(i,j):max(i,j)
+    r += (x[k] - x[k-1]) * (y[k] + y[k-1])
+  end
+  
+  return sign(j-i) * r/2
+end
+
 # Return a `Tuple` consisting of all but the last 2 components of `t`.
 @inline front2(t::Tuple) = _front2(t...)
 _front2() = throw(ArgumentError("Cannot call front2 on an empty tuple."))
 _front2(v) = throw(ArgumentError("Cannot call front2 on 1-element tuple."))
-_front2(v1,v2) = ()
+@inline _front2(v1,v2) = ()
 @inline _front2(v, t...) = (v, _front2(t...)...)
 
 # Return a `Tuple` consisting of all but the first 2 components of `t`.
 @inline tail2(t::Tuple) = _tail2(t...)
 _tail2() = throw(ArgumentError("Cannot call tail2 on an empty tuple."))
 _tail2(v) = throw(ArgumentError("Cannot call tail2 on 1-element tuple."))
-_tail2(v1,v2) = ()
-_tail2(v1, v2, rest...) = rest
+@inline _tail2(v1,v2) = ()
+@inline _tail2(v1, v2, rest...) = rest
 
 # Return a `Tuple` consisting of the first 2 components of `t`.
 @inline first2(t::Tuple) = _first2(t...)
@@ -22,21 +51,26 @@ _first2(v) = throw(ArgumentError("Cannot call first2 on 1-element tuple."))
 @inline last2(t::Tuple) = _last2(t...)
 _last2() = throw(ArgumentError("Cannot call last2 on an empty tuple."))
 _last2(v) = throw(ArgumentError("Cannot call last2 on 1-element tuple."))
-_last2(v1,v2) = (v1, v2)
+@inline _last2(v1,v2) = (v1, v2)
 @inline _last2(v, t...) = _last2(t...)
+
+@inline reverse2(t::Tuple) = _reverse2(t...)
+@inline _reverse2() = throw(ArgumentError("Cannot call reverse2 on an empty tuple."))
+@inline _reverse2(t) = throw(ArgumentError("Cannot call reverse2 on a 1-element tuple."))
+@inline _reverse2(t1,t2,t...) = (t2, t1, t...)
 
 # Return a `Tuple` consisting of a tuple of the first 2 and and a tuple of the last components of `t`.
 @inline front2_last(t::Tuple) = _front2_last(t...)
 _front2_last() = throw(ArgumentError("Cannot call front2_last on an empty tuple."))
 _front2_last(v) = throw(ArgumentError("Cannot call front2_last on 1-element tuple."))
-_front2_last(v1,v2, rest...) = ((v1,v2), rest...)
+@inline _front2_last(v1,v2, rest...) = ((v1,v2), rest...)
 
 # Return a `Tuple` consisting of a tuple of the first and a tuple of the last 2 components of `t`.
 @inline front_last2(t::Tuple) = _front_last2(t...)
 _front_last2() = throw(ArgumentError("Cannot call front_last2 on an empty tuple."))
 _front_last2(v) = throw(ArgumentError("Cannot call front_last2 on 1-element tuple."))
-_front_last2(v1,v2) = ((), (v1, v2))
-_front_last2(v, t...) = tuplejoin((v,), _front_last2(t...)...)
+@inline _front_last2(v1,v2) = ((), (v1, v2))
+@inline _front_last2(v, t...) = tuplejoin((v,), _front_last2(t...)...)
 
 @inline tuplejoin(x::Tuple) = x
 @inline tuplejoin(x::Tuple, y::Tuple) = (x..., y...)
