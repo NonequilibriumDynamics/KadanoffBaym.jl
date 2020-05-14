130df61b4c1acf71914beab6533f02a40163eeed
Francisco Meirinhos <fcmeirinhos@gmail.com>
2020-05-14 12:50:57 +0100

    Deleted old implementation with an interface with OrdinaryDiffEq.jl

--- src/KadanoffBaym.jl
+++ src/KadanoffBaym.jl
@@ -7,20 +7,12 @@ using MuladdMacro
 using Parameters
 using Reexport
 @reexport using EllipsisNotation
-# @reexport using RecursiveArrayTools
-
-# import DiffEqBase: __init, solve!
-# @reexport using OrdinaryDiffEq
 
 include("utils.jl")
 export trapz
 
 include("gf.jl")
 export GreenFunction, Lesser, Greater, MixedLesser, MixedGreater
-
-# include("common.jl")
-# include("core.jl")
-# export KB
 
 include("vcabm.jl")
 include("kb.jl")
