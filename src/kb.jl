24d4f2cc88d6422c765497a4e71edbe3e48addcd
Francisco Meirinhos <fcmeirinhos@gmail.com>
2021-05-28 16:53:20 +0200

    Added some comments and corrected bug that existed on failure of the first step

--- src/kb.jl
+++ src/kb.jl
@@ -16,11 +16,11 @@ for some initial condition `u0` from `t0` to `tmax`.
   - `(t0, tmax)`: the initial and final time
 
 # Optional keyword parameters
-  - `callback(ts, t1)`: A function that gets called everytime the 2-point function values at slice `(t1, 1:t1)` are updated
   - `f_line(u, ts, t1)`: the right-hand-side for 1-point functions
   - `l0`: initial condition for the 1-point functions
   - `kernel_vert(ts, t1, t2, τ)`: the integral kernel of `du/dt1`
   - `kernel_diag(ts, t1, τ)`: the integral kernel of `du/dt1 + du/dt2`
+  - `callback(ts, t1)`: A function that gets called everytime the 2-point function values at slice `(t1, 1:t1)` are updated
   - `kwargs...`: see `VCABMOptions`
 
 # Notes
@@ -30,10 +30,10 @@ for some initial condition `u0` from `t0` to `tmax`.
     Solving Ordinary Differential Equations I: Nonstiff Problems
 """
 function kbsolve(f_vert, f_diag, u0::Vector{<:GreenFunction}, (t0, tmax); 
-  k_vert=nothing, k_diag=nothing, l0=nothing, f_line=nothing, callback=(x...) -> nothing, kwargs...)
+  k_vert=nothing, k_diag=nothing, f_line=nothing, l0=nothing, callback=(x...) -> nothing, kwargs...)
   opts = VCABMOptions(; kwargs...)
 
-  # Support for initial time-grid
+  # Support for an initial time-grid
   if isempty(size(t0))
     t0 = [t0]
   else
@@ -41,19 +41,26 @@ function kbsolve(f_vert, f_diag, u0::Vector{<:GreenFunction}, (t0, tmax);
   end
   @assert last(t0) < tmax "Only t0 < tmax supported"
 
+  # Holds the information about the integration
   state = KBState(u0, t0)
 
+  # Holds the information necessary to time-step
   cache = begin
     t = length(state.t)
     u_prev = VectorOfArray([[x[t, t′] for x in state.u] for t′ in 1:t])
 
     if isnothing(k_vert)
+      # The system is viewed as 1-time ODE whose rhs is the concatenation of all rhs up to time the index `t`
       f = t -> VectorOfArray([[f_vert(state.u, state.t, t, t′) for t′ in 1:(t - 1)]; [f_diag(state.u, state.t, t)]])
     else
+      # The same is done for the Volterra kernels
       k = (t, s) -> VectorOfArray([[k_vert(state.u, state.t, t, t′, s) for t′ in 1:(t - 1)]; [k_diag(state.u, state.t, t, s)]])
-      cache_v = VCABMVolterraCache{eltype(state.t)}(opts.kmax, k(t, t))
+
+      # Holds the information necessary to integrate Volterra kernels
+      cache_v = VCABMVolterraCache{eltype(state.t)}(opts.kmax, typeof(u_prev)(k(t, t).u))
 
       f = t -> begin
+        # Integrates the Volterra kernels v(t) = ₀∫ᵗ ds K(t, s)
         v = quadrature!(cache_v, state.t, s -> k(t, s))
         VectorOfArray([[f_vert(state.u, v[t′], state.t, t, t′) for t′ in 1:(t - 1)]; [f_diag(state.u, v[t], state.t, t)]])
       end
@@ -62,16 +69,10 @@ function kbsolve(f_vert, f_diag, u0::Vector{<:GreenFunction}, (t0, tmax);
     VCABMCache{eltype(state.t)}(opts.kmax, u_prev, typeof(u_prev)(f(t).u))
   end
 
-  # All mutations to user arguments are done explicitely here
   while timeloop!(state, cache, tmax, opts)
     t = length(state.t)
 
-    # Resize solution
-    if t > (last ∘ size ∘ first)(state.u)
-      foreach(u -> resize!(u, t + min(50, ceil(Int, (tmax - state.t[end]) / (state.t[end] - state.t[end - 1])))), state.u)
-    end
-
-    # Extend cache
+    # Extend the cache to accomodate the new time column
     if isnothing(k_vert)
       extend!(cache, state.t, (t, t′) -> f_vert(state.u, state.t, t, t′))
     else
@@ -91,23 +92,23 @@ function kbsolve(f_vert, f_diag, u0::Vector{<:GreenFunction}, (t0, tmax);
     # Calculate error and adjust order
     adjust!(cache, state.t, () -> f(t), opts.kmax, opts.atol, opts.rtol)
   end # timeloop!
-
-  foreach(u -> resize!(u, length(state.t)), state.u) # trim solution
   return state
 end
 
+# Controls the step size & resizes the Green functions if required
 function timeloop!(state, cache, tmax, opts)
-  if isone(cache.k) # first step
+  # Section II.4: Starting Step Size, Eq. (4.14)
+  if isone(length(state.t))
     dt = iszero(opts.dtini) ? initial_step(cache.f_prev, cache.u_prev, opts.atol, opts.rtol) : opts.dtini
-    push!(state.t, last(state.t) + dt)
-    return true
+  else
+    dt = state.t[end] - state.t[end - 1]
   end
 
-  # II.4 Automatic Step Size Control, Equation (4.13)
+  # Section II.4: Automatic Step Size Control, Eq. (4.13)
   q = max(inv(opts.qmax), min(inv(opts.qmin), cache.error_k^(1 / (cache.k + 1)) / opts.γ))
-  dt = min((state.t[end] - state.t[end - 1]) / q, opts.dtmax)
+  dt = min(dt / q, opts.dtmax)
 
-  # Remove t_prev if last step failed
+  # Remove the last element of the time grid if last step failed
   if cache.error_k > one(cache.error_k)
     pop!(state.t)
   end
@@ -119,14 +120,17 @@ function timeloop!(state, cache, tmax, opts)
 
   # Reached the end of the integration
   if iszero(dt) || opts.stop()
+    foreach(u -> resize!(u, length(state.t)), state.u) # trim solution
     return false
   else
+    if length(state.t) == (last ∘ size ∘ first)(state.u) # resize solution
+      foreach(u -> resize!(u, length(state.t) + min(50, ceil(Int, (tmax - state.t[end]) / dt))), state.u)
+    end
     push!(state.t, last(state.t) + dt)
     return true
   end
 end
 
-# Holds the information about the integration
 struct KBState{U,T}
   u::U          # 2-point functions
   t::Vector{T}  # time grid
