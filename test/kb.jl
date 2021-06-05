7605d33721e32ddae54d397cd4c23c9fd5f44cb4
Francisco Meirinhos <fcmeirinhos@gmail.com>
2021-06-05 11:18:57 +0200

    Added 2-time Volterra benchmark

--- test/kb.jl
+++ test/kb.jl
@@ -1,6 +1,6 @@
 @testset "1-time benchmark" begin
   λ = 0.2
-  atol = 1e-8
+  atol = 1e-7
   rtol = 1e-5
 
   function fv(u, ts, t, t′)
@@ -14,7 +14,7 @@
   G = GreenFunction(zeros(ComplexF64, 1, 1), SkewHermitianSymmetry)
   L = GreenFunction(1im * ones(ComplexF64, 1, 1), SkewHermitianSymmetry)
 
-  kb = kbsolve(fv, fd, [G, L], (0.0, 100.0); atol=atol, rtol=rtol)
+  kb = kbsolve(fv, fd, [G, L], (0.0, 30.0); atol=atol, rtol=rtol)
 
   function sol1(t, g0, l0)
     s = sqrt(Complex(λ^2 - 4))
@@ -53,13 +53,9 @@ end
 
   kb = kbsolve(fv, fd, [G], (0.0, 30.0); atol=atol, rtol=rtol, k_vert=kv, k_diag=kd)
 
-  function sol(t)
-    return cos(t) + sin(t)
-  end
+  sol(t) = cos(t) + sin(t)
 
-  @testset begin
-    @test G[:, 1] ≈ [sol(t1) for t1 in kb.t] atol = 1e1atol rtol = 1e1rtol
-  end
+  @test G[:, 1] ≈ [sol(t1) for t1 in kb.t] atol = 1e1atol rtol = 1e1rtol
 end
 
 @testset "2-time benchmark" begin
@@ -79,11 +75,37 @@ end
 
   kb = kbsolve(fv, fd, [L], (0.0, 200.0); atol=atol, rtol=rtol)
 
-  function sol(t, t′)
-    return -1.0im * exp(-1.0im * sin(λ * (t - t′)))
-  end
+  sol(t, t′) = -1.0im * exp(-1.0im * sin(λ * (t - t′)))
 
   @test L.data ≈ [sol(t1, t2) for t1 in kb.t, t2 in kb.t] atol = 1e1atol rtol = 1e1rtol
 end
 
-@testset "2-time Volterra benchmark" begin end
+@testset "2-time Volterra benchmark" begin
+  atol = 1e-8
+  rtol = 1e-5
+
+  function kv(u, times, t, t′, s)
+    return s >= t′ ? [u[1][t′, s]] : zeros(1)
+  end
+  function kd(u, times, t, s)
+    return zeros(1)
+  end
+  function fv(u, v, times, t, t′)
+    return [(1 - v[1])]
+  end
+  function fd(u, v, times, t)
+    return zeros(1)
+  end
+
+  G = GreenFunction(ones(1, 1), SymmetricSymmetry)
+
+  kb = kbsolve(fv, fd, [G], (0.0, 1.0); atol=atol, rtol=rtol, k_vert=kv, k_diag=kd)
+
+  function sol(t)
+    return cos(t) + sin(t)
+  end
+
+  sol_ = hcat([vcat(sol.(kb.t[i] .- kb.t[1:i]), sol.(kb.t[(1 + i):length(kb.t)] .- kb.t[i])) for i in eachindex(kb.t)]...)
+
+  @test G.data ≈ sol_ atol = 1e1atol rtol = 5e2rtol
+end
