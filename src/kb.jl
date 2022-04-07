6027fd10db44b83b28eab08e9dcf6dd83bff26d2
Francisco Meirinhos <fcmeirinhos@gmail.com>
2022-04-07 08:52:36 +0200

    More comments for kb.jl

--- src/kb.jl
+++ src/kb.jl
@@ -3,32 +3,37 @@
 
 Solves the 2-time Voltera integro-differential equation
 
-``du/dt1 = fv = v(u,t1,t2) + ∫_{t0]^{t1} dτ K1v[u,t1,t2,τ] + ∫_{t0}^{t2} dτ K2v[u,t1,t2,τ]``
+``d/dt1 u(t1,t2) = fv(t1,t2) = v[u,t1,t2] + ∫_{t0]^{t1} dτ K1v[u,t1,t2,τ] + ∫_{t0}^{t2} dτ K2v[u,t1,t2,τ]``
 
-``du/dt2 = fh = h(u,t1,t2) + ∫_{t0}^{t1} dτ K1h[u,t1,t2,τ] + ∫_{t0}^{t2} dτ K2h[u,t1,t2,τ]``
+``d/dt2 u(t1,t2) = fh(t1,t2) = h[u,t1,t2] + ∫_{t0}^{t1} dτ K1h[u,t1,t2,τ] + ∫_{t0}^{t2} dτ K2h[u,t1,t2,τ]``
 
-for some initial condition `u0` from `t0` to `tmax`.
+for 2-point functions `u0` from `t0` to `tmax`.
 
 # Parameters
-  - `fv!(out, ts, w1, w2, t1, t2)`: the rhs of `du/dt1` at indices (`t1`, `t2`) 
-    in the time-grid `ts` x `ts`. The weights `w1` and `w2` can be used to integrate
-    the Volterra kernels `K1` and `K2` as `sum_i w1_i K1_i` and  `sum_i w2_i K2_i`,
-    respectively. The output should be saved in-place in `out`, which has the 
-    same shape as `u0`
+  - `fv!(out, ts, w1, w2, t1, t2)`: the rhs of `d/dt1 u` at *indices* (`t1`, `t2`) 
+    in the time-grid (`ts` x `ts`). The weights `w1` and `w2` can be used to integrate
+    the Volterra kernels `K1v` and `K2v` as `sum_i w1_i K1v_i` and  `sum_i w2_i K2v_i`,
+    respectively. The output is saved in-place in `out`, which has the same shape as `u0`.
   - `fd!(out, ts, w1, w2, t1, t2)`: the rhs of `du/dt1 + du/dt2`
   - `u0::Vector{<:GreenFunction}`: list of 2-point functions to be time-stepped
-  - `(t0, tmax)`: a tuple with the initial time(s) `t0` – can be a vector of past times – and final time `tmax`
+  - `(t0, tmax)`: a tuple with the initial time(s) `t0` – can be a vector of 
+    past times – and final time `tmax`
 
 # Optional keyword parameters
-  - `callback(ts, w1, w2, t1, t2)`: A function that gets called everytime the 2-point function at the indices (`t1`, `t2`) is updated
-  - `stop(ts)`: A function that gets called at every step that stops the integration when evaluates to `true`
-  - `atol::Real`: Absolute tolerance (components with magnitude lower than `atol` do not guarantee number of local correct digits)
+  - `callback(ts, w1, w2, t1, t2)`: A function that gets called everytime the 
+    2-point function at *indices* (`t1`, `t2`) is updated. Can be used to update
+    functions which are not being integrated, such as self-energies.
+  - `stop(ts)`: A function that gets called at every time-step that stops the 
+    integration when it evaluates to `true`
+  - `atol::Real`: Absolute tolerance (components with magnitude lower than 
+    `atol` do not guarantee number of local correct digits)
   - `rtol::Real`: Relative tolerance (roughly the local number of correct digits)
   - `dtini::Real`: Initial step-size
   - `dtmax::Real`: Maximal step-size
   - `qmax::Real`: Maximum step-size factor when adjusting the time-step
   - `qmin::Real`: Minimum step-size factor when adjusting the time-step
-  - `γ::Real`: Safety factor for the calculated time-step such that it is accepted with a higher probability
+  - `γ::Real`: Safety factor for the calculated time-step such that it is 
+    accepted with a higher probability
   - `kmax::Integer`: Maximum order of the adaptive Adams method
 
 # Notes
